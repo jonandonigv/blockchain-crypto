fc84f1a0cfaeff235408265b73acea25ae54bc61
jonandonigv <jonandonigv@gmail.com>
2025-10-20 18:43:48 +0200

    wip: Concatenate the blocks and calculate a sha-256 hash on the concatenated combination

--- main.go
+++ main.go
@@ -1,12 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"crypto/sha256"
+	"fmt"
+	"strconv"
+)
 
 type Block struct {
 	Timestamp     int64
 	Data          []byte
 	PrevBlockHash []byte
-	Has           []byte
+	Hash          []byte
+}
+
+func (b *Block) SetHash() {
+	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
+	header := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
+	hash := sha256.Sum256(header)
+
+	b.Hash = hash[:]
 }
 
 func main() {
