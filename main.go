9288dc2f0a35d40ee5f35f9f1adfcacf721a29f5
jonandonigv <jonandonigv@gmail.com>
2025-10-20 19:10:50 +0200

    feature: Blockchain

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	blockchain "github.com/jonandonigv/blockchain-crypto/block-chain"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	bc := blockchain.NewBlockchain()
+
+	bc.AddBlock("Send 1 BTC to Jon")
+	bc.AddBlock("Send 2 more BTC to Jon")
+
+	for _, block := range bc.Blocks {
+		fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %x\n", block.Hash)
+		fmt.Println()
+	}
+
 }
