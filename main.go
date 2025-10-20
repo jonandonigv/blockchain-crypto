3390747b95406ffdff452d8b9b5f792e93f8b131
jonandonigv <jonandonigv@gmail.com>
2025-10-20 18:49:32 +0200

    feature: Block
    Relocated to a block file for ease of use

--- main.go
+++ main.go
@@ -1,33 +1,6 @@
 package main
 
-import (
-	"bytes"
-	"crypto/sha256"
-	"fmt"
-	"strconv"
-	"time"
-)
-
-type Block struct {
-	Timestamp     int64
-	Data          []byte
-	PrevBlockHash []byte
-	Hash          []byte
-}
-
-func (b *Block) SetHash() {
-	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
-	header := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
-	hash := sha256.Sum256(header)
-
-	b.Hash = hash[:]
-}
-
-func NewBlock(data string, prevBlockHash []byte) *Block {
-	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}}
-	block.SetHash()
-	return block
-}
+import "fmt"
 
 func main() {
 	fmt.Println("Hello world!")
