8f13564d70cfb58cb736ab01bc990111cc5d6bd9
jonandonigv <jonandonigv@gmail.com>
2025-10-25 20:43:03 +0200

    wip: Serialize function

--- block/block.go
+++ block/block.go
@@ -104,6 +104,18 @@ func (b *Block) SetHash() {
 	b.Hash = hash[:]
 }
 
+func (b *Block) Serialize() []byte {
+	var result bytes.Buffer
+	enconder := gob.NewEncoder(&result)
+
+	err := enconder.Encode(b)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return result.Bytes()
+}
+
 func DeserializeBlock(d []byte) *Block {
 	var block Block
 	decoder := gob.NewDecoder(bytes.NewReader(d))
