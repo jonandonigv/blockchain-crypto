71042bfdc1f52cb809f7353e68b51919191df1d9
jonandonigv <jonandonigv@gmail.com>
2025-11-08 17:18:17 +0100

    wip: newcoinbase

--- transactions/transaction.go
+++ transactions/transaction.go
@@ -1,5 +1,7 @@
 package transactions
 
+import "fmt"
+
 type Transaction struct {
 	ID   []byte
 	Vin  []TXInput
@@ -15,4 +17,19 @@ type TXInput struct {
 	TxId      []byte
 	Vout      int
 	ScriptSig string
+}
+
+const subsidy = 10
+
+func NewCoinbaseTX(to, data string) *Transaction {
+	if data == "" {
+		data = fmt.Sprintf("Reward to '%s'", to)
+	}
+
+	txin := TXInput{[]byte{}, -1, data}
+	txout := TXOutput{subsidy, to}
+	tx := Transaction{nil, []TXInput{txin}, []TXOutput{txout}}
+	tx.SetID()
+
+	return &tx
 }
