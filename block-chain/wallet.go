7f1693748781f3ddc7bc8930513614ff00743c0e
jonandonigv <jonandonigv@gmail.com>
2025-11-15 18:37:23 +0100

    wip: Wallet

---
+++ block-chain/wallet.go
@@ -0,0 +1,39 @@
+package blockchain
+
+import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"log"
+)
+
+const version = byte(0x00)
+const walletFile = "wallet.dat"
+const addressChecksumLen = 4
+
+type Wallet struct {
+	PrivateKey ecdsa.PrivateKey
+	PublicKey  []byte
+}
+
+type Wallets struct {
+	Wallets map[string]*Wallet
+}
+
+func NewWallet() *Wallet {
+	private, public := newKeyPair()
+	wallet := Wallet{private, public}
+
+	return &wallet
+}
+
+func newKeyPair() (ecdsa.PrivateKey, []byte) {
+	curve := elliptic.P256()
+	private, err := ecdsa.GenerateKey(curve, rand.Reader)
+	if err != nil {
+		log.Panic(err)
+	}
+	pubKey := append(private.PublicKey.X.Bytes(), private.PublicKey.Y.Bytes()...)
+
+	return *private, pubKey
+}
