52e35735b6b91a361c747beedda45a8e97f87db6
jonandonigv <jonandonigv@gmail.com>
2025-11-17 15:11:58 +0100

    wip: Base58Decode

--- block-chain/base58.go
+++ block-chain/base58.go
@@ -1,6 +1,9 @@
 package blockchain
 
-import "math/big"
+import (
+	"bytes"
+	"math/big"
+)
 
 var b58Alphabet = []byte("123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz")
 
@@ -8,6 +11,29 @@ func ReverseBytes(data []byte) {
 	for i, j := 0, len(data)-1; i < j; i, j = i+1, j-1 {
 		data[i], data[j] = data[j], data[i]
 	}
+}
+
+func Base58Decode(input []byte) []byte {
+	result := big.NewInt(0)
+	zeroBytes := 0
+
+	for b := range input {
+		if b == 0x00 {
+			zeroBytes++
+		}
+	}
+
+	payload := input[zeroBytes:]
+	for _, b := range payload {
+		charIndex := bytes.IndexByte(b58Alphabet, b)
+		result.Mul(result, big.NewInt(58))
+		result.Add(result, big.NewInt(int64(charIndex)))
+	}
+
+	decoded := result.Bytes()
+	decoded = append(bytes.Repeat([]byte{byte(0x00)}, zeroBytes), decoded...)
+
+	return decoded
 }
 
 func Base58Encode(input []byte) []byte {
